46bb41d2d352be40e3fcb4ef40f91ff8b23f0ac3
HawkingHuang <math0915@gmail.com>
2024-03-04 00:20:48 +0800

    Install Vuetify and doing test

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <Nav />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Nav from "../components/Nav.vue";
+
+export default {
+  components: {
+    Nav,
+  },
+};
+</script>
+
+<style scoped></style>
